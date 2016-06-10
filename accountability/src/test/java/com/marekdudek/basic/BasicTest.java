713929b9630b68577a9ed3117c3ab6635485d120
Marek Dudek <marek.dudek@gmail.com>
2016-06-10 23:50:31 +0200

    test for people and companies

--- accountability/src/test/java/com/marekdudek/basic/BasicTest.java
+++ accountability/src/test/java/com/marekdudek/basic/BasicTest.java
@@ -2,35 +2,38 @@ package com.marekdudek.basic;
 
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
 
 public class BasicTest {
 
     @Test
-    public void person() {
+    public void people_and_companies() {
 
-        final Person0 person =
-                new Person0(
-                        new TelephoneNumber0("+48507238827"),
-                        new Address0("ul. Polna 7/12, Wroclaw, Poland"),
-                        new EmailAddress0("jan.kowalski@gmail.com")
-                );
+        final Company0 townTaxi   = new Company0(new TelephoneNumber0("+34 (045) 432 1432"), null, null);
+        final Company0 shoemaker  = new Company0(null, new Address0("High Street 12B"), null);
+        final Company0 accountant = new Company0(null, null, new EmailAddress0("john.smith@cpa.org"));
 
-        assertThat(person.getAddress(), is(notNullValue()));
-    }
+        final Person0 wife     = new Person0(new TelephoneNumber0("214 321 124"), null, null);
+        final Person0 mother   = new Person0(new TelephoneNumber0("564 423 754"), null, null);
+        final Person0 son      = new Person0(new TelephoneNumber0("+64 564 423 754"), null, new EmailAddress0("jack@gmail.com"));
+        final Person0 daughter = new Person0(new TelephoneNumber0("+64 564 423 754"), null, new EmailAddress0("kate@gmail.com"));
 
-    @Test
-    public void company() {
+        final Person0 aquaintance = new Person0(null, null, new EmailAddress0("tom.tackson@come-company.com"));
+        final Person0 doctor      = new Person0(null, new Address0("North St. 1D"), null);
+        final Person0 barber      = new Person0(new TelephoneNumber0("643 124 4311"), null, null);
 
-        final Company0 company =
-                new Company0(
-                        new TelephoneNumber0("+480713517503"),
-                        new Address0("pl. Grunwaldzki 8, Wroclaw, Poland"),
-                        new EmailAddress0("biuro@spolkazoo.pl")
-                );
+        final Collection<Company0> companies = Arrays.asList(townTaxi, shoemaker, accountant);
+        final Collection<Person0>  people    = Arrays.asList(wife, mother, son, daughter, aquaintance, doctor, barber);
 
-        assertThat(company.getAddress(), is(notNullValue()));
+        final Collection<Object> addressBook = new ArrayList<>();
+        addressBook.addAll(people);
+        addressBook.addAll(companies);
+
+        assertThat(addressBook, hasSize(10));
     }
 }
